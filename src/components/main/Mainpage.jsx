40681db391d84d1534e8ef752151cc57045a2173
natia <natia.karamnishvili369@law.tsu.ge>
2025-06-09 01:10:20 +0400

    trying to render the screen image

--- src/components/main/Mainpage.jsx
+++ src/components/main/Mainpage.jsx
@@ -17,6 +17,22 @@ function Mainpage(){
       <button className='Mac'>Download for Mac</button>
     </div>
    </section>
+
+   <section className='track'>
+     <div className="description_text">
+      <h2>Keep track of your snippets</h2>
+      <article>
+         Clipboard instantly stores any item you copy in the cloud,
+         meaning you can access your snippets immediately on all your devices.
+         Our Mac and iOS apps will help you organize everything.
+      </article>
+     </div>
+
+     <div className='screen_img&text'>
+      <aside className='screen_img'></aside>
+      <aside className='steps'></aside>
+     </div>
+   </section>
   </>
   )
 }
